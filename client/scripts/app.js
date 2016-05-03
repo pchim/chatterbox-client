7387507008e1f39ac2e12eec5b1e0177bce76f2d
pchim <pchim24@gmail.com>
2016-05-02 20:14:38 -0700

    Implement room selections, message submission and fetches

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,10 +1,22 @@
 // YOUR CODE HERE:
+var results;
+var time;
+var rooms = {allRooms: 'allRooms'};
 var app = {
   init: function () {
     $('.username').on('click', function() {
       app.addFriend();
     });
-    $('#send .submit').on('submit', app.handleSubmit);
+    //$('#send .submit').on('click', function () { return app.handleSubmit(); });
+    $('#send .submit').on('submit', function(event) {
+      app.handleSubmit();
+      event.preventDefault();
+    });
+
+    $('select').change(function(event) {
+      console.log('changed select', $('select').val());
+      changeDom($('select').val());
+    });
 
   },
   send: function (message) {
@@ -15,7 +27,8 @@ var app = {
       data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
-        console.log('chatterbox: Message sent');
+
+        console.log('sent: ', data);
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -28,10 +41,13 @@ var app = {
       // This is the url you should use to communicate with the parse API server.
       url: 'https://api.parse.com/1/classes/messages',
       type: 'GET',
-      data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
-        console.log('chatterbox: Message received');
+        // console.log('fetched', data);
+        //console.log('chatterbox: Message received', data.results);
+        results = data.results;
+        changeDom($('select').val());
+        //time = Date.now();
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -44,9 +60,22 @@ var app = {
     $('#chats').children().remove();
   },
   addMessage: function(message) {
-    $username = $('<div class=\'username\'></div>');
+    time = time ? time : 0;
+    var username = xssFilters.inHTMLData(message['username']);
+    var room = xssFilters.inHTMLData(message['roomname']);
+    var text = xssFilters.inHTMLData(message['text']);
+    // var messageTime = Date.parse(message['createdAt']);
+    // if (messageTime >= time) {
+    $username = $('<div class=\'allRooms ' + room + '\'>' 
+      + '<div class=\'username\'>' + username + ':' + text + ' ' + message['createdAt'] + '</div></div>' );
     $('#chats').append($username);
-
+    //}
+    // time = Date.now();
+    if (!rooms.hasOwnProperty(room)) {
+      rooms[room] = room;
+      $('select').append('<option value=' + room + '>' + room + '</option>');
+    }
+    
   },
   addRoom: function(roomName) {
     $('#roomSelect').append('<div>,</div>');
@@ -55,12 +84,31 @@ var app = {
 
   },
   handleSubmit: function() {
-    console.log('hs called');
+    var message = {
+      username: window.decodeURIComponent(window.location.search.substr(10)),
+      text: $('#message').val(),
+      roomname: 'testRoom'
+    };
+    app.send(message);
   }
 };
 
+var changeDom = function(room) {
+  app.clearMessages();
+  _.each(results, function(item) {
+    if (room !== 'allRooms') {
+      if (room === xssFilters.inHTMLData(item['roomname'])) {
+        app.addMessage(item);
+      }
+    } else {
+      app.addMessage(item);
+    }
+  });
+};
 
+$(document).ready(function() {
 
-
-
-
+  app.init();
+  //console.log($('#postit'));
+  setInterval(function() { app.fetch(); }, 2000);
+});
